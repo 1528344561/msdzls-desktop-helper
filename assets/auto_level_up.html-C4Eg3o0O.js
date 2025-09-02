fd9a7d6d6dec764ff1f152194abce2f554248d3d
1528344561 <1528344561@users.noreply.github.com>
2025-09-02 10:05:22 +0000

    deploy: cc831fe2614fb3b64ab30b00cfa7929286272883

---
+++ assets/auto_level_up.html-C4Eg3o0O.js
@@ -0,0 +1,20 @@
+import{_ as i,c as t,b as s,a,d as e,e as d,r,o as p}from"./app-DYxp6-qW.js";const c={},o={href:"https://ini.msdzls.rainysnow.com/map.ini",target:"_blank",rel:"noopener noreferrer"};function v(u,n){const l=r("ExternalLinkIcon");return p(),t("div",null,[n[3]||(n[3]=s(`<h1 id="自动练级" tabindex="-1"><a class="header-anchor" href="#自动练级"><span>自动练级</span></a></h1><p>该功能并非VIP专享,适合没有小号的玩家给小号升级,用于多开刷材料</p><h2 id="使用步骤" tabindex="-1"><a class="header-anchor" href="#使用步骤"><span>使用步骤</span></a></h2><ol><li>打开桌面版,进入 进阶配置-&gt;自挂功能-&gt;自动挂机配置 页面</li><li>添加需要自动挂机的账号,配置好练级配置,选择挂机地图为 <strong>智能选择</strong></li><li>如果为Free版本,手动双击需要练级的账号</li><li>如果为VIP版本,点击开始按钮,自动批量上号切号挂机</li></ol><p><strong>当升级退出房间时, 挂机会检测到未知状态, 不会立即刷新, 过一会就自动刷新接着打了.</strong></p><p>0.96.0版本地图可能还不够完善,0.96.1起提供全面的地图挂机</p><h2 id="智能选择方案" tabindex="-1"><a class="header-anchor" href="#智能选择方案"><span>智能选择方案</span></a></h2><p>如果提示不存在XX地图,就是你智能选择方案写的有问题</p><h3 id="_0-96-0及之前的格式" tabindex="-1"><a class="header-anchor" href="#_0-96-0及之前的格式"><span>0.96.0及之前的格式</span></a></h3><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text" data-title="text"><pre><code><span class="line">1=色拉陆=3000</span>
+<span class="line">7=神殿=0</span>
+<span class="line">10=色拉陆=0</span>
+<span class="line">15=深渊=0</span>
+<span class="line">18=咖喱日=0</span>
+<span class="line">20=城堡=0</span>
+<span class="line">24=薄荷日=3000</span>
+<span class="line">25=玛奇朵港=0</span>
+<span class="line">41=茴香=7000</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="_0-96-1起新格式-支持全地图" tabindex="-1"><a class="header-anchor" href="#_0-96-1起新格式-支持全地图"><span>0.96.1起新格式[支持全地图]</span></a></h3>`,11)),a("p",null,[n[1]||(n[1]=e("地图名称务必合法(从批量自动挂机窗口的选择挂机地图处复制对应地图名称,或者从",-1)),a("a",o,[n[0]||(n[0]=e("这里",-1)),d(l)]),n[2]||(n[2]=e("复制地图名称),否则无法正常挂机",-1))]),n[4]||(n[4]=s(`<div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text" data-title="text"><pre><code><span class="line">0=曲奇岛=3000</span>
+<span class="line">2=色拉岛(陆)=3000</span>
+<span class="line">7=神殿=0</span>
+<span class="line">10=色拉岛(陆)=0</span>
+<span class="line">15=深渊岛=0</span>
+<span class="line">18=咖喱岛(日)=0</span>
+<span class="line">20=芝士城堡=0</span>
+<span class="line">24=薄荷海滩(日)=3000</span>
+<span class="line">25=玛奇朵港=0</span>
+<span class="line">41=茴香竹筏（日）=0</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1))])}const h=i(c,[["render",v],["__file","auto_level_up.html.vue"]]),b=JSON.parse('{"path":"/docs/guide/auto_level_up.html","title":"自动练级","lang":"en-US","frontmatter":{},"headers":[{"level":2,"title":"使用步骤","slug":"使用步骤","link":"#使用步骤","children":[]},{"level":2,"title":"智能选择方案","slug":"智能选择方案","link":"#智能选择方案","children":[{"level":3,"title":"0.96.0及之前的格式","slug":"_0-96-0及之前的格式","link":"#_0-96-0及之前的格式","children":[]},{"level":3,"title":"0.96.1起新格式[支持全地图]","slug":"_0-96-1起新格式-支持全地图","link":"#_0-96-1起新格式-支持全地图","children":[]}]}],"git":{"createdTime":1742112834000,"updatedTime":1756807429000,"contributors":[{"name":"Jueshi","email":"1528344561@qq.com","commits":6}]},"filePathRelative":"docs/guide/auto_level_up.md"}');export{h as comp,b as data};
