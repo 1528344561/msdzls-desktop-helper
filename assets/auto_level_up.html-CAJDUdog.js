1b65e078ac1e81aca5c3a405a2cb32118f21ab4f
1528344561 <1528344561@users.noreply.github.com>
2026-04-30 12:29:40 +0000

    deploy: def67aa331ffd74d9b3c1c0e6142b997f0faca04

---
+++ assets/auto_level_up.html-CAJDUdog.js
@@ -0,0 +1,22 @@
+import{_ as i,c as d,b as e,a,d as s,e as t,r,o as p}from"./app-DdlxYlgr.js";const c={},o={href:"https://ini.msdzls.rainysnow.com/map.ini",target:"_blank",rel:"noopener noreferrer"};function v(m,n){const l=r("ExternalLinkIcon");return p(),d("div",null,[n[3]||(n[3]=e('<h1 id="自动练级" tabindex="-1"><a class="header-anchor" href="#自动练级"><span>自动练级</span></a></h1><p><strong>该功能并非VIP专享</strong>, 适合没有小号的玩家给小号升级, 用于多开刷材料</p><h2 id="使用步骤" tabindex="-1"><a class="header-anchor" href="#使用步骤"><span>使用步骤</span></a></h2><ol><li>打开桌面版, 在桌面版右中部(开启WPE的地方), 进阶功能-&gt;自挂功能-&gt;自动挂机配置 页面</li><li>添加需要自动挂机的账号, 配置好练级配置, 选择挂机地图为 <strong>智能选择</strong></li><li>如果为Free版本, 手动双击需要练级的账号</li><li>如果为VIP版本, 点击开始按钮, 自动批量上号切号挂机</li></ol><p><strong>当升级退出房间时, 挂机会检测到未知状态, 不会立即刷新, 过一会就自动刷新接着打了</strong></p><h2 id="智能选择方案" tabindex="-1"><a class="header-anchor" href="#智能选择方案"><span>智能选择方案</span></a></h2><p>如果提示不存在XX地图,就是你智能选择方案写的有问题</p>',7)),a("p",null,[n[1]||(n[1]=s("地图名称务必合法(从游戏内的创建房间后中下位置的地图名称复制, 或者从批量自动挂机窗口的选择挂机地图处复制对应地图名称,或者从",-1)),a("a",o,[n[0]||(n[0]=s("这里",-1)),t(l)]),n[2]||(n[2]=s("复制地图名称),否则无法正常挂机",-1))]),n[4]||(n[4]=e(`<div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text" data-title="text"><pre><code><span class="line">0=曲奇岛=3000</span>
+<span class="line">2=色拉岛(陆)=3000</span>
+<span class="line">7=神殿=0</span>
+<span class="line">10=色拉岛(陆)=0</span>
+<span class="line">15=深渊岛=0</span>
+<span class="line">18=咖喱岛(日)=0</span>
+<span class="line">20=芝士城堡=0</span>
+<span class="line">24=薄荷海滩(日)=3000</span>
+<span class="line">25=玛奇朵港=0</span>
+<span class="line">41=茴香竹筏（日）=0</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="q-a" tabindex="-1"><a class="header-anchor" href="#q-a"><span>Q &amp; A</span></a></h2><p>Q: 左边的数字是怎么来的?</p><p>A: 完全是你自己自由设置的, 如 <code>25=玛奇朵港=0</code> 大于等于25级, 刷xxx图, <code>41=茴香竹筏（日）=0</code>大于等于41级 刷xxx图, 你完全可以在中间插入 31=雪顶火山=3000, 表示到了31级就别去刷玛奇朵港了, 去刷雪顶火山, 让方案更精细</p><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text" data-title="text"><pre><code><span class="line">0=曲奇岛=3000</span>
+<span class="line">2=色拉岛(陆)=3000</span>
+<span class="line">7=神殿=0</span>
+<span class="line">10=色拉岛(陆)=0</span>
+<span class="line">15=深渊岛=0</span>
+<span class="line">18=咖喱岛(日)=0</span>
+<span class="line">20=芝士城堡=0</span>
+<span class="line">24=薄荷海滩(日)=3000</span>
+<span class="line">25=玛奇朵港=0</span>
+<span class="line">31=雪顶火山=3000</span>
+<span class="line">41=茴香竹筏（日）=0</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5))])}const b=i(c,[["render",v],["__file","auto_level_up.html.vue"]]),h=JSON.parse('{"path":"/docs/guide/auto_level_up.html","title":"自动练级","lang":"en-US","frontmatter":{},"headers":[{"level":2,"title":"使用步骤","slug":"使用步骤","link":"#使用步骤","children":[]},{"level":2,"title":"智能选择方案","slug":"智能选择方案","link":"#智能选择方案","children":[]},{"level":2,"title":"Q & A","slug":"q-a","link":"#q-a","children":[]}],"git":{"createdTime":1742112834000,"updatedTime":1777477478000,"contributors":[{"name":"Jueshi","email":"1528344561@qq.com","commits":10}]},"filePathRelative":"docs/guide/auto_level_up.md"}');export{b as comp,h as data};
